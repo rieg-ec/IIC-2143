511edd59fd1e7c6d0919e164e6efa07df76d62a0
rieg-ec <rieg@uc.cl>
2021-09-26 16:59:57 -0300

    feat(Gemfile): add cors policies

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,20 @@ Bundler.require(*Rails.groups)
 
 module HobbiesApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '/public/*', headers: :any, methods: :get
+        resource '/api/*',
+          headers: :any,
+          expose: ['X-Page', 'X-PageTotal'],
+          methods: [:get, :post, :patch, :put, :delete, :options]
+      end
+    end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.load_defaults 5.2
+    config.time_zone = 'America/Santiago'
+    config.assets.paths << Rails.root.join('node_modules')
 
     config.generators do |g|
       g.test_framework :rspec, fixture: true
