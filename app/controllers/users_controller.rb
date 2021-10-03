acc94e94db2c842cd2143fc24e3f4799d64c7cca
rieg-ec <rieg@uc.cl>
2021-10-02 23:33:00 -0300

    feat(user): empty controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index; end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  resources :users, only: [:index, :show, :edit, :update]
+
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
