1ee6f14c71361335a943398c36c4aaee65de4888
rieg-ec <rieg@uc.cl>
2021-10-05 23:53:25 -0300

    fix(devise): redirect loop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@
 
 class UsersController < ApplicationController
   include Pundit
-  before_action :authenticate_user!
 
   def index
     @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show edit update]
 
-  devise_for :users
+  root to: 'home#index'
+
+  devise_for :users, path_prefix: 'accounts'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'home#index'
 end
