dc5faefc2191ba5b56b26643725f80b5ba6f4375
rieg-ec <rieg@uc.cl>
2021-10-08 11:04:04 -0300

    fix(user): pundit wrong usage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  include Pundit
   before_action :authenticate_user!
 
   def index
-    authorize(User)
-
     @users = User.all
   end
 
