70b94e59663ec89cc134660dfe03846df96c6dec
vtunon <vtunon@uc.cl>
2021-11-04 01:25:22 -0300

    avance landing

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue/dist/vue.esm';
 import { camelizeKeys } from 'humps';
+import VueCarousel from 'vue-carousel';
 
 import PopularCourses from './components/popular-courses.vue';
 import CourseInfo from './components/courses/course-info.vue';
@@ -12,6 +13,7 @@ Vue.filter('camelizeKeys', (value) => camelizeKeys(value));
 Vue.component('popular-courses', PopularCourses);
 Vue.component('course-info', CourseInfo);
 Vue.component('course-lesson', CourseLesson);
+Vue.use(VueCarousel);
 
 export default () => {
   document.addEventListener('DOMContentLoaded', () => {
