c3d3498841ac60da6342d797bf042b9271cd9e6d
rieg-ec <rieg@uc.cl>
2021-11-22 19:04:06 -0300

    fix(lecture): outdated serializer & bug fix

--- app/javascript/src/components/courses/course-lecture.vue
+++ app/javascript/src/components/courses/course-lecture.vue
@@ -8,33 +8,6 @@
         >
       </video>
     </div>
-    <div class="flex flex-row justify-between w-full pt-8">
-      <span class="text-2xl font-semibold">Preguntas</span>
-      <button
-        @click="openReviewModal = true"
-        class="main-btn"
-      >
-        Preguntar
-      </button>
-    </div>
-    <div class="flex flex-col w-full space-y-2">
-      <div
-        class="relative flex flex-col justify-between w-full h-full px-4 py-12 border"
-        v-for="(question, index) in questions"
-        :key="index"
-      >
-        <p class="">
-          {{ question.body }}
-        </p>
-        <span class="absolute bottom-0 left-0 px-4 py-2 text-sm text-base font-normal text-gray-500">{{ question.createdAt }}</span>
-      </div>
-    </div>
-    <question-modal
-      @cancel="openReviewModal = false"
-      @confirm="handleQuestionCreate"
-      v-if="openReviewModal"
-      :course-id="course.id"
-    />
   </div>
 </template>
 
@@ -42,9 +15,7 @@
 export default {
   name: 'CourseLecture',
   props: {
-    course: { type: Object, required: true },
     lecture: { type: Object, required: true },
-    questions: { type: Array, default: () => [] },
   },
   data() {
     return {
